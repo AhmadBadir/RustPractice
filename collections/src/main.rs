cb2adc08881f6846b91f0cc0ca749bd18ac879b8
AhmadBadir <bdair2002@gmail.com>
2025-01-08 17:20:24 +0100

    hashmaps

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let v : Vec<i32> = Vec::new();
 
@@ -148,11 +150,59 @@ fn main() {
         }
     //print bytes
      for c in hello.bytes() {
-         println!(byte is {c}");
+         println!("byte is {c}");
+         }
+     println!("hello is {hello}");
+     //hashmaps: create new hashmap and insert
+     let mut scores = HashMap::new();
+
+     scores.insert(String::from("Blue"), 10);
+     scores.insert(String::from("Yellow"), 50);
+
+     let team_name = String::from("Blue");
+     let score = scores.get(&team_name).copied().unwrap_or(0);
+     println!("score from hashmap is {score}");
+
+     for(key, value) in &scores {
+         println!("{key} : {value}");
          }
 
+     //ownsership in a hashmap
+     let field_name = String::from("Favorite color");
+     let field_value = String::from("Blue");
 
+     let mut map = HashMap::new();
+     map.insert(field_name, field_value);
+     //println!("field_name String cannot be used here {field_name}"); //code won't compile, field ownership has moved
 
+     //updating values in hashmap
+     // method one - overwrite update
+     let mut scores = HashMap::new();
+
+     scores.insert(String::from("Blue"), 10);
+     scores.insert(String::from("Blue"), 25);
+
+     println!("{scores:?}");
+
+     //method 2: adding key and value only if a key isn't present
+     let mut scores = HashMap::new();
+
+     scores.insert(String::from("Blue"), 10);
+     scores.entry(String::from("Yellow")).or_insert(50);
+     scores.entry(String::from("Blue")).or_insert(50);
+     println!("{scores:?}");
+
+     //method 3 : updating a value based on the old value
+     let text = "hello world wonderful world";
+
+     let mut map = HashMap::new();
+
+     for word in text.split_whitespace() {
+         let count = map.entry(word).or_insert(0);
+         *count += 1;
+         }
+     println!("{map:?}");
+     println!("original text is {text}");
 }
 // signature for + operator is
 // fn add(self, s: &str) -> String {
