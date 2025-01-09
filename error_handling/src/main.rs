721e8526516ea1466b223079eeaa7277836d0965
AhmadBadir <bdair2002@gmail.com>
2025-01-09 18:27:14 +0100

    types of panics

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
     //panic - unrecoverable errors
-    panic!("Crash and burn");
+    //panic!("Crash and burn");
     //println!("Hello, world!");
+
+    let v = vec![1,2,3];
+    //program will panice due to index out of bound
+    //v[99];
 }
