5bb695f3fed5945cd840c8e822d146f6d046f286
AhmadBadir <bdair2002@gmail.com>
2025-01-10 11:26:42 +0100

    recovering errors initial code

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -6,4 +6,14 @@ fn main() {
     let v = vec![1,2,3];
     //program will panice due to index out of bound
     //v[99];
+
+
+    //recoverable errors
+    use std::fs::File;
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {error:?}"),
+        };
 }
