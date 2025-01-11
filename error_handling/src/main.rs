70c0233fae83617a7a2451f7dc7691abec4c14b1
AhmadBadir <bdair2002@gmail.com>
2025-01-11 10:33:19 +0100

    recoverable errors using match

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,5 @@
+ use std::fs::File;
+ use std::io::ErrorKind;
 fn main() {
     //panic - unrecoverable errors
     //panic!("Crash and burn");
@@ -8,12 +10,29 @@ fn main() {
     //v[99];
 
 
-    //recoverable errors
-    use std::fs::File;
+    //recoverable errors using panic (program will crash)
+
+
+//     let greeting_file_result = File::open("hello.txt");
+//
+//     let greeting_file = match greeting_file_result {
+//         Ok(file) => file,
+//         Err(error) => panic!("Problem opening the file: {error:?}"),
+//         };
+
+    //recoverable errors using non panic approach
     let greeting_file_result = File::open("hello.txt");
 
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {error:?}"),
-        };
+        let greeting_file = match greeting_file_result {
+            Ok(file) => file,
+            Err(error) => match error.kind() {
+                ErrorKind::NotFound => match File::create("hello.txt") {
+                    Ok(fc) => fc,
+                    Err(e) => panic!("Problem creating the file: {e:?}"),
+                    },
+                    other_error => {
+                        panic!("Problem opening the file: {other_error:?}");
+                        }
+                },
+            };
 }
